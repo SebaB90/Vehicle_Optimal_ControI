489be06b70517f4525aae92389701f26fafd80f1
antoplg <antorapa00@gmail.com>
2023-12-14 01:22:28 +0100

    lol

--- Project.py
+++ Project.py
@@ -8,11 +8,14 @@
 import numpy as np
 import matplotlib as plt
 
-# TASK 0: DISCRETIZATION
+##### TASK 0: DISCRETIZATION #####
 
-ns = 6
-ni = 2
-dt = 1e-3
+
+#define params
+
+dt = 1e-3   #sample time
+ns = 6      #number of states
+ni = 2      #number of inputs
 
 m = 1480    #Kg
 Iz = 1950   #Kg*m^2
@@ -21,11 +24,9 @@ b = 1.029   #m
 mi = 1      #nodim
 g = 9.81    #m/s^2
 
-u = np.zeros((ni,))
-x_plus = np.zeros((ns,))
-x = np.zeros((ns,))
-
 def dynamics (x, u):
+
+    x_plus = np.ones((ns,))
     Beta = [u[0] - (x[3]*np.sin(x[4]) + a*x[5])/(x[3]*np.sin(x[4])), - (x[3]*np.sin(x[4]) - b*x[5])/(x[3]*np.sin(x[4]))]    # Beta = [Beta_f, Beta_r]
     Fz = [m*g*b/(a+b), m*g*a/(a+b)]             # Fz = [F_zf, F_zr]
     Fy = [mi*Fz[0]*Beta[0], mi*Fz[1]*Beta[1]]   # Fy = [F_yf, F_yr]
@@ -37,31 +38,29 @@ def dynamics (x, u):
     x_plus[4] = x[4] + dt*((Fy[1] * np.cos(x[4]) + Fy[0] * np.cos(x[4] - u[0]) - u[1] * np.sin(x[4] - u[0]))/(m * x[3]) - x[5])  # Beta dot
     x_plus[5] = x[5] + dt*(((u[1] * np.sin(u[0]) + Fy[0] * np.cos(u[0])) * a - Fy[1] * b)/Iz)                       # Psi dot dot
 
-    fx = np.array([1, 0, 0, 0, 0, 0],
+    fx = np.array([[1, 0, 0, 0, 0, 0],
                   [0, 1, 0, 0, 0, 0],
                   [dt*(-x[3] * np.cos(x[4]) * np.sin(x[2]) - x[3] * np.sin(x[4]) * np.cos(x[2])), dt*(x[3] * np.cos(x[4]) * np.cos(x[2]) - x[3] * np.sin(x[4]) * np.sin(x[2])), 1, 0, 0, 0],
-                  [dt*(np.cos(x[4]) * np.cos(x[2]) - np.sin(x[4]) * np.sin(x[2])), dt*(np.cos(x[4]) * np.sin(x[2]) + np.sin(x[4]) * np.cos(x[2])), 0, 1, -(1/(m*(x[3]^2)))*dt*((Fy[1] * np.cos(x[4]) + Fy[0] * np.cos(x[4] - u[0]) - u[1] * np.sin(x[4] - u[0]))), 0],
+                  [dt*(np.cos(x[4]) * np.cos(x[2]) - np.sin(x[4]) * np.sin(x[2])), dt*(np.cos(x[4]) * np.sin(x[2]) + np.sin(x[4]) * np.cos(x[2])), 0, 1, -(1/(m*(x[3]**2)))*dt*((Fy[1] * np.cos(x[4]) + Fy[0] * np.cos(x[4] - u[0]) - u[1] * np.sin(x[4] - u[0]))), 0],
                   [dt*(x[3] * -np.sin(x[4]) * np.cos(x[2]) - x[3] * np.cos(x[4]) * np.sin(x[2])), dt*(x[3] * -np.sin(x[4]) * np.sin(x[2]) + x[3] * np.cos(x[4]) * np.cos(x[2])), 0, dt*((Fy[1] * np.cos(x[4]) + u[1] * - np.sin(x[4] - u[0]) + Fy[0] * np.cos(x[4] - u[0]))/m), 1 + dt*((Fy[1] * -np.sin(x[4]) + Fy[0] * np.sin(x[4] - u[0]) + u[1] * np.cos(x[4] - u[0]))/(m * x[3])), 0],
-                  [0, 0, dt, 0, -1, 1])
+                  [0, 0, dt, 0, -1, 1]])
     
-    fu = np.array([0, 0, 0, dt*(u[1] * np.sin(x[4] - u[0]) + Fy[0] * -np.cos(x[4] - u[0]))/m, dt*(Fy[0] * np.sin(x[4] - u[0]) + u[1] * np.cos(x[4] - u[0]))/(m * x[3]),  dt*((u[1] * np.cos(u[0]) - Fy[0] * np.sin(u[0])) *a/Iz)],
-                  [0, 0, 0, dt*np.cos(x[4] - u[0])/m, dt*(- np.sin(x[4] - u[0]))/(m * x[3]), dt * np.sin(u[0]) * a/Iz])
+    fu = np.array([[0, 0, 0, dt*(u[1] * np.sin(x[4] - u[0]) + Fy[0] * -np.cos(x[4] - u[0]))/m, dt*(Fy[0] * np.sin(x[4] - u[0]) + u[1] * np.cos(x[4] - u[0]))/(m * x[3]),  dt*((u[1] * np.cos(u[0]) - Fy[0] * np.sin(u[0])) *a/Iz)],
+                  [0, 0, 0, dt*np.cos(x[4] - u[0])/m, dt*(- np.sin(x[4] - u[0]))/(m * x[3]), dt * np.sin(u[0]) * a/Iz]])
 
     return x_plus, fx, fu
 
 
-# TASK 1: TRAJECTORY GENERATION
+##### TASK 1: TRAJECTORY GENERATION #####
 
 # We have to find the eqilibria for the system, a way to do that is to use the cornering equilibria, those associated to the systems with Betadot, Vdor and Psidodot = 0
 # Once I have set them I can concentrate on the last three equation, then imposing Veq and PsidotEq (I can think of this also as Veq/R with R a certain imposed radious) we obtain Betaeq, Fxeq and Deltaeq, in alternative I can set Veq and Betaeq and as concequence find the other eqilibrium values.
 # The associated x and y trajectory can then be obtained by forward integration of the dynamics with the values we just found.
 # For vehicles these trajectories are called corering eqilibria, in which I have circles with some radious and some Veq.
 
-x_plus = dynamics(x,u)
 
-x_plus[3]=0
-x_plus[4]=0
-x_plus[5]=0
+u = np.array([1, 1])
+x = np.array([1, 1, 1, 1, 1, 1])
 
 fx, fu = dynamics(x, u)[1:]
 
