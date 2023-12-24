0f795f6ffeeed6c26b6334d692c5c77675477a06
SebaB90 <sebastiano.bertame@studio.unibo.it>
2023-12-24 18:35:57 +0100

    Equiibrium

--- Project.py
+++ Project.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 ##### TASK 0: DISCRETIZATION #####
 
@@ -33,12 +34,12 @@ def dynamics (x, u):
     Fz = [m*g*b/(a+b), m*g*a/(a+b)]             # Fz = [F_zf, F_zr]
     Fy = [mi*Fz[0]*Beta[0], mi*Fz[1]*Beta[1]]   # Fy = [F_yf, F_yr]
 
-    x_plus[0] = x[0] + dt*(x[3] * np.cos(x[4]) * np.cos(x[2]) - x[3] * np.sin(x[4]) * np.sin(x[2]))                 # X dot
-    x_plus[1] = x[1] + dt*(x[3] * np.cos(x[4]) * np.sin(x[2]) + x[3] * np.sin(x[4]) * np.cos(x[2]))                 # Y dot
-    x_plus[2] = x[2] + dt*x[5]                                                                                      # Psi dot
-    x_plus[3] = x[3] + dt*((Fy[1] * np.sin(x[4]) + u[1] * np.cos(x[4] - u[0]) + Fy[0] * np.sin(x[4] - u[0]))/m)     # V dot
-    x_plus[4] = x[4] + dt*((Fy[1] * np.cos(x[4]) + Fy[0] * np.cos(x[4] - u[0]) - u[1] * np.sin(x[4] - u[0]))/(m * x[3]) - x[5])  # Beta dot
-    x_plus[5] = x[5] + dt*(((u[1] * np.sin(u[0]) + Fy[0] * np.cos(u[0])) * a - Fy[1] * b)/Iz)                       # Psi dot dot
+    x_plus[0] = x[0] + dt*(x[3] * np.cos(x[4]) * np.cos(x[2]) - x[3] * np.sin(x[4]) * np.sin(x[2]))                                 # X dot
+    x_plus[1] = x[1] + dt*(x[3] * np.cos(x[4]) * np.sin(x[2]) + x[3] * np.sin(x[4]) * np.cos(x[2]))                                 # Y dot
+    x_plus[2] = x[2] + dt*x[5]                                                                                                      # Psi dot
+    x_plus[3] = x[3] + dt*((Fy[1] * np.sin(x[4]) + u[1] * np.cos(x[4] - u[0]) + Fy[0] * np.sin(x[4] - u[0]))/m)                     # V dot
+    x_plus[4] = x[4] + dt*((Fy[1] * np.cos(x[4]) + Fy[0] * np.cos(x[4] - u[0]) - u[1] * np.sin(x[4] - u[0]))/(m * x[3]) - x[5])     # Beta dot
+    x_plus[5] = x[5] + dt*(((u[1] * np.sin(u[0]) + Fy[0] * np.cos(u[0])) * a - Fy[1] * b)/Iz)                                       # Psi dot dot
 
     fx = np.array([[1, 0, 0, 0, 0, 0],
                   [0, 1, 0, 0, 0, 0],
@@ -61,13 +62,15 @@ def dynamics (x, u):
 # For vehicles these trajectories are called corering eqilibria, in which I have circles with some radious and some Veq.
 
 u = np.array([0, 0])
-x = np.array([10, 0, 0, 1, 0, 0])
+x = np.array([0, 0, 0, 1, 0, 0])
 
 x_plus, fx, fu = dynamics(x, u)
 
 A = fx.T
 B = fu.T
 
+
+# OPEN LOOP TEST to check if the dynamics do what expected ---------------------------------------------------
 x_traj = [x[0]]
 y_traj = [x[1]]
 
@@ -89,6 +92,7 @@ plt.grid(True)
 plt.show()
 
 
+# CHECK IF THE DERIVATIVES ARE CORRECT ----------------------------------------------------------------------
 xx = np.zeros((ns,))
 ddx = np.zeros((ns,))
 
@@ -101,3 +105,32 @@ diff = xx_plus - x_plus
 check = diff - np.dot(fx,ddx)
 
 print (check)
+
+# Evaluate the equilibrium
+
+#eq1                            
+x3=1
+x4=0
+
+def equations(vars):
+    u0, u1, x5 = vars
+    Beta = [u0 - (x3*np.sin(x4) + a*x5)/(x3*np.cos(x4)), - (x3*np.sin(x4) - b*x5)/(x3*np.cos(x4))]    # Beta = [Beta_f, Beta_r]
+    Fz = [m*g*b/(a+b), m*g*a/(a+b)]             # Fz = [F_zf, F_zr]
+    Fy = [mi*Fz[0]*Beta[0], mi*Fz[1]*Beta[1]]   # Fy = [F_yf, F_yr]
+   
+    eq1 = x3 + dt*((Fy[1] * np.sin(x4) + u1 * np.cos(x4 - u0) + Fy[0] * np.sin(x4 - u0))/m)                     # V dot
+    eq2 = x4 + dt*((Fy[1] * np.cos(x4) + Fy[0] * np.cos(x4 - u0) - u1 * np.sin(x4 - u0))/(m * x3) - x5)     # Beta dot
+    eq3 = x5 + dt*(((u1 * np.sin(u0) + Fy[0] * np.cos(u0)) * a - Fy[1] * b)/Iz)                                       # Psi dot dot
+
+    return [eq1, eq2, eq3]
+
+# Initial guess for the solution
+initial_guess = [0, 0, 0]
+
+# Use fsolve to find the solution
+solution = fsolve(equations, initial_guess)
+
+# Print the result
+print(f'Solution: {solution}')
+
+
